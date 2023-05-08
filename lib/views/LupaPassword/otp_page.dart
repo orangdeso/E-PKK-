53eaf2bbcde240845d1d77e1df3e2f043cd40f44
orangdeso <ahmadzaqi98mmm@gmail.com>
2023-05-08 20:21:33 +0700

    update riwayat

---
+++ lib/views/LupaPassword/otp_page.dart
@@ -0,0 +1,224 @@
+import 'package:e_pkk/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class otpPage extends StatefulWidget {
+  const otpPage({super.key});
+
+  @override
+  State<otpPage> createState() => _otpPageState();
+}
+
+class _otpPageState extends State<otpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        //elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios_new,
+          color: grey900,
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Align(
+              alignment: FractionalOffset.topLeft,
+              child: Text(
+                "Verifikasi Kode",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Text(
+              "Periksa nomor whatsapp Anda untuk memverifikasi kode otp",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  height: 68,
+                  width: 55,
+                  child: TextFormField(
+                    onSaved: (pin1) {},
+                    onChanged: (value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "0",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    style: Theme.of(context).textTheme.headline6,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(1),
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 68,
+                  width: 55,
+                  child: TextFormField(
+                    onSaved: (pin2) {},
+                    onChanged: (value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "0",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    style: Theme.of(context).textTheme.headline6,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(1),
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 68,
+                  width: 55,
+                  child: TextFormField(
+                    onSaved: (pin3) {},
+                    onChanged: (value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "0",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    style: Theme.of(context).textTheme.headline6,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(1),
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 68,
+                  width: 55,
+                  child: TextFormField(
+                    onSaved: (pin4) {},
+                    onChanged: (value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "0",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    style: Theme.of(context).textTheme.headline6,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(1),
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 68,
+                  width: 55,
+                  child: TextFormField(
+                    onSaved: (pin5) {},
+                    onChanged: (value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "0",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    style: Theme.of(context).textTheme.headline6,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(1),
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 68,
+                  width: 55,
+                  child: TextFormField(
+                    onSaved: (pin6) {},
+                    onChanged: (value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "0",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    style: Theme.of(context).textTheme.headline6,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(1),
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Belum menerima kode ? "),
+              GestureDetector(
+                onTap: () {},
+                child: Text(" Kirim ulang"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
